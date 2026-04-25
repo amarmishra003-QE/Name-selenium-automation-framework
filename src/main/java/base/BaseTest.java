cf79a94acdcae7f8b3140918247f61e52b6e6253
Amar Mishra <amarmishra003@gmail.com>
2026-04-26 05:03:03 +0530

    Update BaseTest.java
    Add BaseTest implementation for WebDriver setup and teardown.

--- src/main/java/base/BaseTest.java
+++ src/main/java/base/BaseTest.java
@@ -1,1 +1,39 @@
-Add BaseTest class for driver setup and teardown
+package base;
+
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.util.Properties;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class BaseTest {
+
+    public static WebDriver driver;
+    public static Properties prop;
+
+    public void setUp() {
+        try {
+            prop = new Properties();
+            FileInputStream fis = new FileInputStream("config.properties");
+            prop.load(fis);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        String browser = prop.getProperty("browser");
+
+        if (browser.equalsIgnoreCase("chrome")) {
+            driver = new ChromeDriver();
+        }
+
+        driver.get(prop.getProperty("baseUrl"));
+        driver.manage().window().maximize();
+    }
+
+    public void tearDown() {
+        if (driver != null) {
+            driver.quit();
+        }
+    }
+}

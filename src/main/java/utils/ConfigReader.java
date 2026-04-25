51afc3f5b43d3f35b53dcebb6e863c31c2089ad1
Amar Mishra <amarmishra003@gmail.com>
2026-04-26 05:05:50 +0530

    Update ConfigReader.java

--- src/main/java/utils/ConfigReader.java
+++ src/main/java/utils/ConfigReader.java
@@ -1,1 +1,24 @@
-Add ConfigReader utility for reading config properties
+package utils;
+
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.util.Properties;
+
+public class ConfigReader {
+
+    Properties prop;
+
+    public ConfigReader() {
+        prop = new Properties();
+        try {
+            FileInputStream fis = new FileInputStream("config.properties");
+            prop.load(fis);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public String getProperty(String key) {
+        return prop.getProperty(key);
+    }
+}

ed4fe70df33bb551045d415aa4a5c9c7b2ff75d7
Amar Mishra <amarmishra003@gmail.com>
2026-04-26 05:05:17 +0530

    Update LoginTest.java

--- src/test/java/tests/LoginTest.java
+++ src/test/java/tests/LoginTest.java
@@ -1,1 +1,31 @@
-src/test/java/tests/LoginTest.java
+package tests;
+
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Test;
+
+import base.BaseTest;
+import pages.LoginPage;
+
+public class LoginTest extends BaseTest {
+
+    LoginPage loginPage;
+
+    @BeforeMethod
+    public void setUpTest() {
+        setUp();
+        loginPage = new LoginPage(driver);
+    }
+
+    @Test
+    public void testLogin() {
+        loginPage.enterUsername("testuser");
+        loginPage.enterPassword("password");
+        loginPage.clickLogin();
+    }
+
+    @AfterMethod
+    public void tearDownTest() {
+        tearDown();
+    }
+}

56a591b2f4043a5beb1627e37faadfbbeb4e3e29
Amar Mishra <amarmishra003@gmail.com>
2026-08-25 05:44:32 +0530

    Update BaseTest.java

--- src/main/java/base/BaseTest.java
+++ src/main/java/base/BaseTest.java
@@ -2,16 +2,20 @@ package base;
 
 import java.io.FileInputStream;
 import java.io.IOException;
+import java.time.Duration;
 import java.util.Properties;
 
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.BeforeMethod;
 
 public class BaseTest {
 
     public static WebDriver driver;
     public static Properties prop;
 
+    @BeforeMethod
     public void setUp() {
         try {
             prop = new Properties();
@@ -21,16 +25,23 @@ public class BaseTest {
             e.printStackTrace();
         }
 
-        String browser = prop.getProperty("browser");
+        // Case-insensitive check to avoid null issues
+        String browser = prop.getProperty("Browser", "chrome");
 
         if (browser.equalsIgnoreCase("chrome")) {
             driver = new ChromeDriver();
         }
 
-        driver.get(prop.getProperty("baseUrl"));
         driver.manage().window().maximize();
+        
+        // Timeout handling
+        int timeout = Integer.parseInt(prop.getProperty("timeout", "10"));
+        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(timeout));
+
+        driver.get(prop.getProperty("url"));
     }
 
+    @AfterMethod
     public void tearDown() {
         if (driver != null) {
             driver.quit();

f8e8f9ca8341d5b188a4bbaddbcc1ce60f2bc0c7
Amar Mishra <amarmishra003@gmail.com>
2026-08-25 05:47:00 +0530

    Update LoginPage.java

--- src/main/java/pages/LoginPage.java
+++ src/main/java/pages/LoginPage.java
@@ -2,28 +2,37 @@ package pages;
 
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+import java.time.Duration;
 
 public class LoginPage {
 
-    WebDriver driver;
+    private final WebDriver driver;
+    private final WebDriverWait wait;
+
+    // Locators
+    private final By usernameField = By.id("username");
+    private final By passwordField = By.id("password");
+    private final By loginButton = By.id("login");
 
     public LoginPage(WebDriver driver) {
         this.driver = driver;
+        this.wait = new WebDriverWait(driver, Duration.ofSeconds(10));
     }
 
-    By username = By.id("username");
-    By password = By.id("password");
-    By loginBtn = By.id("login");
-
-    public void enterUsername(String user) {
-        driver.findElement(username).sendKeys(user);
+    public LoginPage enterUsername(String user) {
+        wait.until(ExpectedConditions.visibilityOfElementLocated(usernameField)).sendKeys(user);
+        return this;
     }
 
-    public void enterPassword(String pass) {
-        driver.findElement(password).sendKeys(pass);
+    public LoginPage enterPassword(String pass) {
+        wait.until(ExpectedConditions.visibilityOfElementLocated(passwordField)).sendKeys(pass);
+        return this;
     }
 
-    public void clickLogin() {
-        driver.findElement(loginBtn).click();
+    public HomePage clickLogin() {
+        wait.until(ExpectedConditions.elementToBeClickable(loginButton)).click();
+        return new HomePage(driver); // Returns next page object
     }
 }
